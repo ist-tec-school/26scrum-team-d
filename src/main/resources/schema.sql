9ff6973b66c460fa1b607dac480c2ae3743cb1aa
KangChimyong-ist <chimyong.kang@is-tech.co.jp>
2026-04-20 17:30:04 +0900

    データ型の整合と更新時バグの修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,13 @@
-CREATE TABLE IF NOT EXISTS tasklist (
-    id   VARCHAR(8)  PRIMARY KEY,
-    task VARCHAR(256),
-    task_user_id INT,
-    deadline VARCHAR(10),
-    done BOOLEAN,
-    FOREIGN KEY (task_user_id) REFERENCES users(user_id)
-    );
-
 CREATE TABLE IF NOT EXISTS users(
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100)
     );
+
+CREATE TABLE IF NOT EXISTS tasklist (
+    id   VARCHAR(8)  PRIMARY KEY,
+    task VARCHAR(256),
+    task_user VARCHAR(256),
+    deadline VARCHAR(10),
+    done BOOLEAN
+    );
+
