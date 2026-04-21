722e8d10cdc5544c64eecb2717d42b5266bb2c08
masahirobito <masahiro.bito@is-tech.co.jp>
2026-04-21 16:00:43 +0900

    タスクを期限が近い順に変更

--- src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
+++ src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
@@ -78,8 +78,7 @@ public class TaskListDao {
         }
 
         String placeholders = String.join(",", statusList.stream().map(s -> "?").toList());
-
-        // ③ SQL文を組み立てる。最後に ORDER BY を追加！
+        
         String query = "SELECT * FROM tasklist WHERE done IN (" + placeholders + ") ORDER BY deadline ASC";
 
         List<Map<String, Object>> result = jdbcTemplate.queryForList(query, statusList.toArray());
