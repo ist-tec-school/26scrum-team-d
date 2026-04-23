58d36d6f4860750e135f78e198c7eefc78f6de6b
masahirobito <masahiro.bito@is-tech.co.jp>
2026-04-23 18:29:45 +0900

    修正

--- src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
+++ src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
@@ -84,10 +84,11 @@ public class TaskListDao {
         }
 
         if (keyword != null && !keyword.isBlank()) {
-            sql.append(" AND (LOWER(t.task) LIKE LOWER(?) OR LOWER(u.name) LIKE LOWER(?))");
+            sql.append(" AND (LOWER(t.task) LIKE LOWER(?) OR LOWER(p.project_name) LIKE LOWER(?) OR LOWER(u.name) LIKE LOWER(?))");
             String wildcardKeyword = "%" + keyword + "%";
             params.add(wildcardKeyword); // task 用
             params.add(wildcardKeyword); // u.name 用
+            params.add(wildcardKeyword);
         }
 
         sql.append(" ORDER BY t.deadline ASC");
