418dbc6ba53563e04817a088e152c39160f71162
yu-go-o148 <yumi.seno@is-tech.co.jp>
2026-05-13 10:28:47 +0900

    ログイン画面に新規ユーザー登録画面への遷移リンクを作成

--- src/main/java/jp/gihyo/projava/tasklist/SignupController.java
+++ src/main/java/jp/gihyo/projava/tasklist/SignupController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SignupController {
 
-    // "/signup" というURLにアクセスが来たら、signup.html を表示させる設定
     @GetMapping("/signup")
     public String displaySignup() {
         return "signup";
