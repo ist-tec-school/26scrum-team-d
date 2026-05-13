1de72bfd42ebd084e13956f0576e99c234c2e1ba
KangChimyong-ist <chimyong.kang@is-tech.co.jp>
2026-05-13 13:50:18 +0900

    最新developmentのmerge

--- src/main/java/jp/gihyo/projava/tasklist/SignupController.java
+++ src/main/java/jp/gihyo/projava/tasklist/SignupController.java
@@ -1,13 +1,30 @@
 package jp.gihyo.projava.tasklist;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class SignupController {
+    private final TaskListDao dao;
+
+    @Autowired // コンストラクタでインジェクション
+    public SignupController(TaskListDao dao) {
+        this.dao = dao;
+    }
 
     @GetMapping("/signup")
-    public String displaySignup() {
+    public String displaySignup(Model model) {
+        List<Map<String, Object>> departments = dao.findAllDepartments();
+        List<Map<String, Object>> sections = dao.findAllSections();
+
+        model.addAttribute("departments",departments);
+        model.addAttribute("sections",sections);
+
         return "signup";
     }
 }
--- src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
+++ src/main/java/jp/gihyo/projava/tasklist/TaskListDao.java
@@ -198,4 +198,5 @@ public class TaskListDao {
                 })
                 .toList();
     }
+
 }
