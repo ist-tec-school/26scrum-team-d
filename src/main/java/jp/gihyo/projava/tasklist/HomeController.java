831a40624a413471e4d13a33db65609d640af4aa
KangChimyong-ist <chimyong.kang@is-tech.co.jp>
2026-05-13 11:21:07 +0900

    development merge

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -209,4 +209,8 @@ public class HomeController {
     public String login() {
         return "login";
     }
+    @GetMapping("/signup")
+    public String showSignupPage() {
+        return "signup";
+    }
 }
