c12ea1856d7b0439fb0d45d62920d1d30cb3193c
TakutoYoshida3 <takuto.yoshida@is-tech.co.jp>
2026-05-20 16:09:00 +0900

    アプリケーション（Java全体）が認識する標準時間を、サーバーの場所に関係なく「日本時間（JST）」に完全に固定

--- src/main/java/jp/gihyo/projava/tasklist/TaskListApplication.java
+++ src/main/java/jp/gihyo/projava/tasklist/TaskListApplication.java
@@ -2,9 +2,17 @@ package jp.gihyo.projava.tasklist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class TaskListApplication {
+
+	// アプリケーション起動時に、JVM全体のデフォルトタイムゾーンを日本時間に固定する
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tokyo"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskListApplication.class, args);
