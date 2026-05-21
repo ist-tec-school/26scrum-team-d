983e1cb2c271e4b0a247f00956de37412576b15c
TakutoYoshida3 <takuto.yoshida@is-tech.co.jp>
2026-05-21 18:10:19 +0900

    CSRF防御の有効化

--- src/main/java/jp/gihyo/projava/tasklist/SecurityConfig.java
+++ src/main/java/jp/gihyo/projava/tasklist/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+//                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(
                         "/**/*.css",
